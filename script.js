02b1a5e0fb5f5b2354dbebd5a282b154b51d4e71
Brian <BMArsenault@gmail.com>
2021-11-11 17:23:43 -0700

    Mapbox access token

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-var axios = require("axios").default;
+// MapBox Api
 
-var options = {
-  method: 'GET',
-  url: 'https://trailapi-trailapi.p.rapidapi.com/trails/explore/',
-  params: {lat: '<REQUIRED>', lon: '<REQUIRED>'},
-  headers: {
-    'x-rapidapi-host': 'trailapi-trailapi.p.rapidapi.com',
-    'x-rapidapi-key': '84debd863fmshfa58b4e1f335a6ap154ee5jsn70f312469b28'
-  }
-};
+// https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json
+// access token:  pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// var URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+// var endPoint = ".json?limit=5&access_token="
+// var accessToken = "pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
 
-// var url = https://prescriptiontrails.org/api/filter/?by=city&city=
 
-// var endpoint = &offset=0&count=6
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://trailapi-trailapi.p.rapidapi.com/trails/explore/',
+//   params: {lat: '33.4484° N', lon: '112.0740° W'},
+//   headers: {
+//     'x-rapidapi-host': 'trailapi-trailapi.p.rapidapi.com',
+//     'x-rapidapi-key': '84debd863fmshfa58b4e1f335a6ap154ee5jsn70f312469b28'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+// var url = "https://prescriptiontrails.org/api/filter/?by=city&city=""
+
+// var endpoint = "&offset=0&count=6"
+// var queryURLhttps://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6
+// fetch()
