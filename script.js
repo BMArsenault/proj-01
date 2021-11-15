903b4e1e212bb7679ff7ddcce6eefd98daa10464
Brian <BMArsenault@gmail.com>
2021-11-15 12:47:53 -0700

    Added script to html and updated var in js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 var cityInputEl = "Hiking"
 
 function fetchCity(parameter) {
-    var baseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/hiking.json?limit=5&access_token="
+    var baseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+    var endPoint = ".json?limit=5&access_token="
     var apiKey = "pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
-    var url = baseUrl + apiKey + parameter; // not sure of order or exact url used
+    var url = baseUrl + parameter + endPoint + apiKey; // not sure of order or exact url used
 
     fetch(url)
     .then(function (response) {
@@ -16,6 +17,8 @@ function fetchCity(parameter) {
 var searches = [];
 var userInput = document.querySelector("#city");
 var savedList = document.querySelector("#history");
+var currentCity = document.querySelector("#searched-City");
+
 
 // get a list of saved searches using local storage and create buttons
 function getSearches() {
@@ -34,6 +37,16 @@ function getSearches() {
         savedList.appendChild(div);
     }
 }
+
+//  create a list with searched cities
+
+// Have map post for the city we just searched
+// Find a way to post map into "iframe"
+function renderSearchResults(result) {
+    currentCity.innerHTML = ""
+
+}
+
 
 // save to input to localStorage
 var input = document.getElementById('saveServer').value;
