8af66eb5a9e0436654e2450b92690a54a03ff3f1
Brian <BMArsenault@gmail.com>
2021-11-15 16:18:05 -0700

    Added iframe append and eventListener for search button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
-var cityInputEl = "Hiking"
+var searches = [];
 
 function fetchCity(parameter) {
+    // mapbox.places/ allows us to search city instead of lat/long but does not show up on a webpage
     var baseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
     var endPoint = ".json?limit=5&access_token="
     var apiKey = "pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
@@ -14,7 +14,7 @@ function fetchCity(parameter) {
     .then(renderSearchResults)
 }
 
-var searches = [];
+
 var userInput = document.querySelector("#city");
 var savedList = document.querySelector("#history");
 var currentCity = document.querySelector("#searched-City");
@@ -41,17 +41,24 @@ function getSearches() {
 //  create a list with searched cities
 
 // Have map post for the city we just searched
-// Find a way to post map into "iframe"
+// Create iframe and post into HTML
 function renderSearchResults(result) {
     currentCity.innerHTML = ""
-
+    var loadMap = document.getElementById("cityMap")
+    var iframe = document.createElement("iframe")
+    iframe.setAttribute("src", "https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA")
+    loadMap.appendChild(iframe)
 }
 
+document.getElementById("searchbtn").addEventListener("click", function() {
+    loadMap.appendChild(iframe);
+})
 
-// save to input to localStorage
-var input = document.getElementById('saveServer').value;
-localStorage.setItem('server', input);
 
+
+// save to input to localStorage to grab for weather and map
+// var input = document.getElementById('saveServer').value;
+// localStorage.setItem('server', input);
 
 //and to get the text back we would use
 //(document.getElementById('saveServer').value = localStorage.getItem('server');)
@@ -65,10 +72,9 @@ localStorage.setItem('server', input);
 //           Added links for mapbox
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Preview
-// full styleUrl = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt.html?title=view&access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA&zoomwheel=true&fresh=true#9.55/37.278/-112.7652
-
 // Map styled for hiking 
-//style URL = mapbox://styles/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt
+// full styleUrl = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt.html?title=view&access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA&zoomwheel=true&fresh=true#9.55/33.4484/112.0740
+
+// style URL = mapbox://styles/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt
 // Access Token = pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
-// third party = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
+// Url w/ lat and long = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
