2a7737c20174604d161ad20af023eda2442cd728
Brian <BMArsenault@gmail.com>
2021-11-16 15:17:27 -0700

    Testing fetch city in function

--- script.js
+++ script.js
@@ -1,60 +1,94 @@
-var searches = [];
-
-function fetchCity(parameter) {
-    // mapbox.places/ allows us to search city instead of lat/long but does not show up on a webpage
-    var baseUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
-    var endPoint = ".json?limit=5&access_token="
-    var apiKey = "pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
-    var url = baseUrl + parameter + endPoint + apiKey; // not sure of order or exact url used
-
-    fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(renderSearchResults)
-}
+// var searches = [];
+// var savedList = document.getElementById("#history");
+// var currentCity = document.getElementById("#searched-city");
 
 
-var userInput = document.querySelector("#city");
-var savedList = document.querySelector("#history");
-var currentCity = document.querySelector("#searched-City");
+fetch("http://dev.virtualearth.net/REST/v1/LocalSearch/?query=Locations=seattle&key=Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl")
+.then((response) => {
+    console.log("We got response");
+    console.log(response);
+    return response.json();
+})
+.then((jsonData) => {
+    console.log("We got our data");
+    console.log(jsonData)
+});
+
+// function fetchCity(city) {
+    
+//     var baseUrl = "http://dev.virtualearth.net/REST/v1/Locations?q=";
+//     var endPoint = "&output=xml&key=";
+//     var apiKey = "Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl";
+//     var url = baseUrl + ${city} + endPoint + apiKey;
+
+//     fetch(url)
+//     .then(response => response.json());
+        
+//     })
+//     .then(renderSearchResults) => {
+//         console.log("We got our data")
+//         console.log(renderSearchResults)
+// }
+// }
+
+// function renderSearchResults(result) {
+
+//     currentCity.innerHTML = ""
+
+//     var myMap = document.getElementById("cityMap")
+//     var iframe = document.createElement("iframe")
+//     iframe.classList("map")
+//     iframe.setAttribute("src", "http://dev.virtualearth.net/REST/v1/Locations?q=${parameter}&output=xml&key=Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl")
+//     iframe.appendChild(myMap)
+
+// }
 
 
-// get a list of saved searches using local storage and create buttons
-function getSearches() {
-    savedList.innerHTML = "";
-    for (var i = 0; i < searches.length; i++) {
-        var search = searches[i];
-        var div = document.createElement("div");
-        var button = document.createElement("button");
 
-        button.textContent = search;
-        button.classList.add("btn");
-        button.onClick = handleLiClick
-        div.setAttribute("data-index", i);
 
-        div.appendChild(button);
-        savedList.appendChild(div);
-    }
-}
+
+
+
+
+
+
+// // add eventlistener to button
+// const button = document.getElementById("searchbtn");
+
+// button.addEventListener("click", savedCity);
+
+
+// //  store and get city to and from localStorage
+// function getCity() {
+//     return localStorage.getItem("searchedCity")
+// }
+
+// function savedCity() {
+//     var city = document.getElementById("searched-city").value;
+
+//     localStorage.setItem("searchCity", city);
+//     // console.log("Clicked Button");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  create a list with searched cities
 
 // Have map post for the city we just searched
 // Create iframe and post into HTML
-function renderSearchResults(result) {
-    currentCity.innerHTML = ""
-    var loadMap = document.getElementById("cityMap")
-    var iframe = document.createElement("iframe")
-    iframe.setAttribute("src", "https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA")
-    loadMap.appendChild(iframe)
-}
-
-document.getElementById("searchbtn").addEventListener("click", function() {
-    loadMap.appendChild(iframe);
-})
-
-
 
 // save to input to localStorage to grab for weather and map
 // var input = document.getElementById('saveServer').value;
@@ -65,16 +99,26 @@ document.getElementById("searchbtn").addEventListener("click", function() {
 
 
 
+// bing maps
+// var url = "http://dev.virtualearth.net/REST/v1/Locations?q="
+// parameter = city searched
+// var endPoint = "&output=xml&key=""
+// var apiKey = "Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl"
+// http://dev.virtualearth.net/REST/v1/Locations?q=seattle&output=xml&key=Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl
 
-
-
-
-//           Added links for mapbox
+//           Graveyard
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// national park maps
+// var baseUrl = "https://developer.nps.gov/";
+// var endPoint = "api/v1/parks";
+// var apiKey = "o1FOO1q63Y41fwWArlMIJN1hREzMFioFeTTpACSt";
+
+
+// mapbox maps
+// "https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
 // Map styled for hiking 
 // full styleUrl = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt.html?title=view&access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA&zoomwheel=true&fresh=true#9.55/33.4484/112.0740
-
 // style URL = mapbox://styles/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt
 // Access Token = pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
-// Url w/ lat and long = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA
+// Url w/ lat and long = https://api.mapbox.com/styles/v1/bmarsenault/ckvz3cc8717zt15rzmqhb1yxt/wmts?access_token=pk.eyJ1IjoiYm1hcnNlbmF1bHQiLCJhIjoiY2t2dmlqNTd3NmUzdDMxczE3eDZhbWZ6cSJ9.Xdpp-ALevFxYRQnHx5BwhA"
