7c6fa12350c3535512aab779beddafd4f7294a65
Brian <BMArsenault@gmail.com>
2021-11-16 17:44:31 -0700

    Added weather

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-var searches = [];
-var savedList = document.getElementById("#history");
-var currentCity = document.getElementById("#searched-city");
+// var searches = [];
+// var savedList = document.getElementById("#history");
+// var currentCity = document.getElementById("#searched-city");
 
 
-const url = 'http://dev.virtualearth.net/REST/v1/LocalSearch/?query=Locations=${query}&key=Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl';
-function fetchCity(city) {
+// function fetchCity(city) {
     
-    var baseUrl = "http://dev.virtualearth.net/REST/v1/Locations?q=";
-    var endPoint = "&output=xml&key=";
-    var apiKey = "Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl";
-    var url = baseUrl + city + endPoint + apiKey;
+//     var baseUrl = "http://dev.virtualearth.net/REST/v1/Locations?q=";
+//     var endPoint = "&output=xml&key=";
+//     var apiKey = "Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl";
+//     var url = baseUrl + city + endPoint + apiKey;
 
-    fetch(url)
-    .then(response => response.json())
-    .then((renderSearchResults) => {
-        console.log(renderSearchResults)
-}
+//     fetch(url)
+//     .then(response => response.json())
+//     .then((renderSearchResults) => {
+//         console.log(renderSearchResults)
+//     });
+// }
+
 
 // function renderSearchResults(result) {
 
@@ -32,24 +33,115 @@ function fetchCity(city) {
 
 
 // // add eventlistener to button
-const button = document.getElementById("searchbtn");
+// const button = document.getElementById("searchbtn");
 
-button.addEventListener("click", savedCity);
+// button.addEventListener("click", savedCity);
 
 
-//  store and get city to and from localStorage
-function getCity() {
-    return localStorage.getItem("searchedCity")
+// //  store and get city to and from localStorage
+// function getCity() {
+//     return localStorage.getItem("searchedCity")
+// }
+
+// function savedCity() {
+//     var city = document.getElementById("searched-city").value;
+
+//     localStorage.setItem("searchCity", city);
+//     // console.log("Clicked Button");
+// };
+
+var searchEl = document.getElementById("search");
+var cityInputEl = document.getElementById("city-name");
+
+// search function
+var searchHandler = function(event) {
+    debugger;
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+    console.log(cityName);
+    if (cityName) {
+        saveSearch();
+        getCurrentWeather();
+        cityInputEl.value = "";
+    } else {
+        alert("Please search for a city.");
+    }
+};
+
+// fetch and display location weather
+var getCurrentWeather = function() {
+    // format the github api url
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.value.trim() + '&appid=e58ce6fcd378144b93c4b6f45a5073c8';
+  
+    // make a get request to url
+    fetch(apiUrl)
+      .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function(data) {
+            console.log(data);
+            displayWeather(data)
+            getCityCoordinates(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function(error) {
+        alert('Unable to connect to OpenWeather.');
+      });
+  };
+
+  function displayWeather(data) {
+    // temp,  wind, humidity
+    var temp = Math.round(((data.main.temp - 273.15) * 1.8) + 32);
+    var wind = data.wind.speed;
+    var humidity = data.main.humidity;
+    console.log(temp);
+    console.log(wind);
+    console.log(humidity);
+    
+    var tempEl = document.getElementById("temp");
+    tempEl.innerHTML = temp;
+
+    var windEl = document.getElementById("wind");
+    windEl.innerHTML = wind;
+
+    var humidityEl = document.getElementById("humidity");
+    humidityEl.innerHTML = humidity;
+
+    var cityNameEl = document.getElementById("search-term");
+    cityNameEl.innerHTML = data.name + ", " + data.sys.country;
+
+    if (data.length === 0) {
+        cityInfoEl.textContent = "No city found.";
+        return;
+    };
 }
 
-function savedCity() {
-    var city = document.getElementById("searched-city").value;
+function getCityCoordinates(data) {
+    var longitude = data.coord.lon;
+    var latitude = data.coord.lat;
+    console.log(longitude);
+    console.log(latitude);
+    // generate map from coordinates
+};
 
-    localStorage.setItem("searchCity", city);
-    // console.log("Clicked Button");
-}
+function saveSearch() {
+    var cityList = JSON.parse(localStorage.getItem("City-List"));
+    if (!cityList) {
+        savedItems.push(cityName);        
+        localStorage.setItem("City-List", JSON.stringify(savedItems));
+    } else {
+        cityList = JSON.parse(localStorage.getItem("City-List"));
+        savedItems = cityList;
+        savedItems.push(cityName);        
+        localStorage.setItem("City-List", JSON.stringify(savedItems));
+    };
+};    
 
-
+searchEl.addEventListener("submit", searchHandler);
 
 
 
@@ -59,6 +151,7 @@ function savedCity() {
 // var apiKey = "o1FOO1q63Y41fwWArlMIJN1hREzMFioFeTTpACSt";
 
 
+// const url = 'http://dev.virtualearth.net/REST/v1/LocalSearch/?query=Locations=${query}&key=Aqy96zlGbO5ltS3p1E6aAKMleaGy0i_tu7rKfSRVU2KNtGBZIgpR87I5x61ct6Bl';
 
 
 
